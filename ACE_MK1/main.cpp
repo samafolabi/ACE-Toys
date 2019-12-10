f0e3f2b6c46651156f294c53e9cf98f824ef2393
Samson Afolabi <samafolabi2001@gmail.com>
2019-12-10 00:19:11 +0000

    Wheel and Motor Control added

--- ACE_MK1/main.cpp
+++ ACE_MK1/main.cpp
@@ -3,56 +3,79 @@
 #include "TextLCD.h"
 #include "motordriver.h"
 
-TextLCD lcd(D2, D3, D4, D5, D6, D7); // rs, e, d4-d7
-Motor A(D10, D8, D9, 1);
-Motor B(D12, D14, D15, 1);
+//TextLCD lcd(D2, D3, D4, D5, D6, D7); // rs, e, d4-d7
+//enable, fwd, rev
+Motor A(D10, D8, D9, 1); //left
+Motor B(D12, D14, D15, 1); //right
+Serial pc(USBTX, USBRX);
+//Serial wifi(PE_8,PE_7);
 
+void fwd();
+void rev();
+void left();
+void right();
+void stop();
+void waits(float x) {wait_us(x*1000000);};
 
 int main()
 {
-	
+    pc.baud(9600);
 	while(1)
 	{
-        lcd.cls();
-		lcd.printf("Forward");
-        A.speed(0.8);
-        B.speed(0.8);
-        wait_us(2000000);
-
-        A.stop(0.5);
-        B.stop(0.5);
-        wait_us(50000);
+        pc.printf("Forward\n\r");
+        fwd();
+        waits(2.0);
+        stop();
+        waits(0.5);
         
-        lcd.cls();
-		lcd.printf("Backward");
-        A.speed(-0.8);
-        B.speed(-0.8);
-        wait_us(2000000);
-
-        A.stop(0.5);
-        B.stop(0.5);
-        wait_us(50000);
+        pc.printf("Backward\n\r");
+        rev();
+        waits(2.0);
+        stop();
+        waits(0.5);
         
-        lcd.cls();
-		lcd.printf("Right");
-        A.speed(0.8);
-        B.speed(-0.8);
-        wait_us(2000000);
-
-        A.stop(0.5);
-        B.stop(0.5);
-        wait_us(50000);
+        pc.printf("Right\n\r");
+        right();
+        waits(2.0);
+        stop();
+        waits(0.5);
         
-        lcd.cls();
-		lcd.printf("Left");
-        A.speed(-0.8);
-        B.speed(0.8);
-        wait_us(2000000);
-
-        A.stop(0.5);
-        B.stop(0.5);
-        wait_us(50000);
-        
+        pc.printf("Left\n\r");
+        left();
+        waits(2.0);
+        stop();
+        waits(0.5);
 	}
 	
+}
+
+void fwd()
+{
+    A.speed(0.8);
+    B.speed(-0.8);
+}
+
+void rev()
+{
+    A.speed(-0.8);
+    B.speed(0.8);
+}
+
+void left()
+{
+    A.speed(-0.8);
+    B.speed(-0.8);
+}
+
+void right()
+{
+    A.speed(0.8);
+    B.speed(0.8);
+}
+
+void stop()
+{
+    A.stop(0.5);
+    B.stop(0.5);
+    waits(0.5);
 }
