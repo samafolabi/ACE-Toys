bfe262edf55f8c6c46e44f64119bc783fd11cd23
Samson Afolabi <samafolabi2001@gmail.com>
2019-12-10 02:06:37 +0000

    Added Wi-Fi test code

--- ACE_MK1/main.cpp
+++ ACE_MK1/main.cpp
@@ -8,7 +8,7 @@
 Motor A(D10, D8, D9, 1); //left
 Motor B(D12, D14, D15, 1); //right
 Serial pc(USBTX, USBRX);
-//Serial wifi(PE_8,PE_7);
+Serial wifi(PE_8,PE_7);
 
 void fwd();
 void rev();
@@ -16,37 +16,52 @@ void left();
 void right();
 void stop();
 void waits(float x) {wait_us(x*1000000);};
+void motor_test();
 
 int main()
 {
-    pc.baud(9600);
+    pc.baud(115200);
+    wifi.baud(115200);
 	while(1)
 	{
-        pc.printf("Forward\n\r");
-        fwd();
-        waits(2.0);
-        stop();
-        waits(0.5);
-        
-        pc.printf("Backward\n\r");
-        rev();
-        waits(2.0);
-        stop();
-        waits(0.5);
-        
-        pc.printf("Right\n\r");
-        right();
-        waits(2.0);
-        stop();
-        waits(0.5);
-        
-        pc.printf("Left\n\r");
-        left();
-        waits(2.0);
-        stop();
-        waits(0.5);
+        if (pc.readable())
+        {
+            wifi.putc(pc.getc());
+        }
+        if (wifi.readable())
+        {
+            pc.putc(wifi.getc());
+        }
+        //motor_test();
 	}
 	
+}
+
+void motor_test()
+{
+    pc.printf("Forward\n\r");
+    fwd();
+    waits(2.0);
+    stop();
+    waits(0.5);
+    
+    pc.printf("Backward\n\r");
+    rev();
+    waits(2.0);
+    stop();
+    waits(0.5);
+    
+    pc.printf("Right\n\r");
+    right();
+    waits(2.0);
+    stop();
+    waits(0.5);
+    
+    pc.printf("Left\n\r");
+    left();
+    waits(2.0);
+    stop();
+    waits(0.5);
 }
 
 void fwd()
